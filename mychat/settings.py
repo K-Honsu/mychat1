6ae550b87f9aba73242a902661c0dff3637e245f
P-Horus <feranmia51@gmail.com>
2023-01-03 01:18:33 +0100

    final commit

--- mychat/settings.py
+++ mychat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_MYCHAT')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mychat1.vercel.app/']
 
 
 # Application definition
